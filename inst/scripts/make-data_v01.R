0bdcc31905837b994a85bb8edc9a9fe7d5742e47
kumeS <satoshi.kume@riken.jp>
2021-04-21 13:16:55 +0900

    first version

---
+++ inst/scripts/make-data_v01.R
@@ -0,0 +1,17 @@
+###################################################################
+#Data Dump Version: ??th Apr 2021 - JST from Wikidata Dump
+###################################################################
+##Downloads on Mac Terminal
+#wget https://dumps.wikimedia.org/wikidatawiki/entities/latest-truthy.nt.bz2
+##Decompress the bz2 file
+#bunzip2 latest-truthy.nt.bz2
+##Chnage the file name
+#mv latest-truthy.nt latest-truthy-2104XX.nt
+
+#Alternative way: Downloads from Public_Repository_for_AHwikidataDbs
+
+
+
+
+
+
