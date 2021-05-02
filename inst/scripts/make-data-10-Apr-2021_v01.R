acfc060a20a46aa7da8edd2a0dd36730f4ea7e05
kumeS <satoshi.kume@riken.jp>
2021-05-02 23:37:17 +0900

    first version

--- inst/scripts/make-data-10-Apr-2021_v01.R
+++ inst/scripts/make-data-10-Apr-2021_v01.R
@@ -72,81 +72,40 @@ dir()
 ##For Relation_P279_P31.nt
 ######################################################
 #system("open ../WikidataRDF-10-Apr-2021")
+#source("../AHWikiDataDbs/inst/scripts/make-data_v02.R")
 File_path <- "./Relation_P279_P31.nt"
-con_file <- file(description = File_path, open = "r")
-con_file
-#close(con_file)
 
-#Execution number
-x <- 0
-#Rows per read
-N <- 100000
-
-#Purse NT file to CSV
-while( TRUE ){
-x <- x + 1
-print(paste0("No: ", x, " Line: ", x*N ))
-try(a <- readLines(con_file, n = N), silent=T)
-if ( length(a) == 0 ) { close(con_file); break }
-#head(a)
-
-#AiBiCiDiEiFiG: Unique arbitrary string
-a1 <- unlist(strsplit(sub(" ", "AiBiCiDiEiFiG", sub(" ", "AiBiCiDiEiFiG", a)), split="AiBiCiDiEiFiG"))
-if( (length(a1) %% 3) == 0 ){
-  b <- matrix(a1, ncol=3, byrow=T)
-}else{
-  b <- matrix(a1[1:(length(a1) - (length(a1) %% 3))], ncol=3, byrow=T)
-}
-
-#head(b)
-# remove [.]
-b[,3] <- sub(" [.]$", "", b[,3])
-
-#Subject
-cc <- grepl("^<http://www.wikidata.org/entity/Q[0-9]", b[,1])
-b[cc,1] <- sub("^<http://www.wikidata.org/entity/", "wd:", b[cc,1])
-b[cc,1] <- sub(">$", "", b[cc,1])
-b <- b[grepl("^wd:Q", b[,1]),]
-
-#Property
-cc <- grepl("^<http://www.wikidata.org/prop/direct/P[0-9]", b[,2])
-b[cc,2] <- sub("^<http://www.wikidata.org/prop/direct/", "wdt:", b[cc,2])
-b[cc,2] <- sub(">$", "", b[cc,2])
-b <- b[grepl("^wdt:P", b[,2]),]
-
-#Object
-cc <- grepl("^<http://www.wikidata.org/entity/Q[0-9]", b[,3])
-b[cc,3] <- sub("^<http://www.wikidata.org/entity/", "wd:", b[cc,3])
-b[cc,3] <- sub(">$", "", b[cc,3])
-b <- b[grepl("^wd:Q", b[,3]),]
-
-#head(b)
-#dim(b)
-
-#convert matrix to DF
-d <- data.frame(b)
-
-if(x == 1){
-readr::write_csv(d,
-                 file=paste0(sub(".nt", "", File_path), "_df.csv"),
-                 append=FALSE, col_names = FALSE)
-}else{
-readr::write_csv(d,
-                 file=paste0(sub(".nt", "", File_path), "_df.csv"),
-                 append=TRUE, col_names = FALSE)
-}}
+#Run
+PurseNT_ClassHierarchy(File_path)
 #close(con_file)
 
 #Check the data
-Dat <- data.frame(readr::read_csv(paste0(sub(".nt", "", File_path), "_df.csv")))
+Dat <- data.frame(readr::read_csv(paste0(sub(".nt", "", File_path), "_df.csv"), col_names = FALSE))
+head(Dat)
+dim(Dat)
+sum(grepl("^wd:", Dat[,1]))
+sum(grepl("^wdt:", Dat[,2]))
+sum(grepl("^wd:", Dat[,3]))
+system.time(saveRDS(Dat, file = paste0(sub(".nt", "", File_path), "_df.Rdata")))
+#Time: 102.385 sec
+#rm(list=ls())
+
+system.time(data.frame(readr::read_csv(paste0(sub(".nt", "", File_path), "_df.csv"), col_names = FALSE)))
+#Time: 72.664 sec
+system.time(readRDS(file = paste0(sub(".nt", "", File_path), "_df.Rdata")))
+#Time: 90.694 sec
 
 
-saveRDS(Dat, file = paste0(sub(".nt", "", File_path), "_df.Rdata"))
+######################################################
+##For Label_en_rdfs.nt
+######################################################
+#system("open ../WikidataRDF-10-Apr-2021")
+#source("../AHWikiDataDbs/inst/scripts/make-data_v02.R")
+File_path <- "./Label_en_rdfs.nt"
 
+#Run
+PurseNT_Label(File_path)
+#close(con_file)
 
-
-
-
-
-
+#Check the data
 
---
+++ inst/scripts/make-data_v02.R
@@ -0,0 +1,128 @@
+
+PurseNT_ClassHierarchy <- function(File_path){
+con_file <- file(description = File_path, open = "r")
+print(con_file)
+#close(con_file)
+
+#Execution number
+x <- 0
+#Rows per read
+N <- 100000
+
+#Purse NT file to CSV
+while( TRUE ){
+x <- x + 1
+print(paste0("No: ", x, " Line: ", x*N ))
+try(a <- readLines(con_file, n = N), silent=T)
+if ( length(a) == 0 ) { close(con_file); break }
+#head(a)
+#AiBiCiDiEiFiG: Unique arbitrary string
+a1 <- unlist(strsplit(sub(" ", "AiBiCiDiEiFiG", sub(" ", "AiBiCiDiEiFiG", a)), split="AiBiCiDiEiFiG"))
+if( (length(a1) %% 3) == 0 ){
+  b <- matrix(a1, ncol=3, byrow=T)
+}else{
+  b <- matrix(a1[1:(length(a1) - (length(a1) %% 3))], ncol=3, byrow=T)
+}
+
+#head(b)
+# remove [.]
+b[,3] <- sub(" [.]$", "", b[,3])
+
+#Subject
+cc <- grepl("^<http://www.wikidata.org/entity/Q[0-9]", b[,1])
+b[cc,1] <- sub("^<http://www.wikidata.org/entity/", "wd:", b[cc,1])
+b[cc,1] <- sub(">$", "", b[cc,1])
+b <- b[grepl("^wd:Q", b[,1]),]
+
+#Property
+cc <- grepl("^<http://www.wikidata.org/prop/direct/P[0-9]", b[,2])
+b[cc,2] <- sub("^<http://www.wikidata.org/prop/direct/", "wdt:", b[cc,2])
+b[cc,2] <- sub(">$", "", b[cc,2])
+b <- b[grepl("^wdt:P", b[,2]),]
+
+#Object
+cc <- grepl("^<http://www.wikidata.org/entity/Q[0-9]", b[,3])
+b[cc,3] <- sub("^<http://www.wikidata.org/entity/", "wd:", b[cc,3])
+b[cc,3] <- sub(">$", "", b[cc,3])
+b <- b[grepl("^wd:Q", b[,3]),]
+
+#head(b)
+#dim(b)
+
+#convert matrix to DF
+d <- data.frame(b)
+
+if(x == 1){
+readr::write_csv(d,
+                 file=paste0(sub(".nt", "", File_path), "_df.csv"),
+                 append=FALSE, col_names = FALSE)
+}else{
+readr::write_csv(d,
+                 file=paste0(sub(".nt", "", File_path), "_df.csv"),
+                 append=TRUE, col_names = FALSE)
+}}
+}
+
+############################################################
+############################################################
+PurseNT_Label <- function(File_path){
+con_file <- file(description = File_path, open = "r")
+print(con_file)
+#close(con_file)
+
+#Execution number
+x <- 0
+#Rows per read
+N <- 100000
+
+while( TRUE ){
+x <- x + 1
+print(paste0("No: ", x, " Line: ", x*N ))
+try(a <- readLines(con_file, n = N), silent=T)
+if ( length(a) == 0 ) { close(con_file); break }
+
+#head(a)
+a1 <- unlist(strsplit(sub(" ", "AiBiCiDiEiFiG", sub(" ", "AiBiCiDiEiFiG", a)), split="AiBiCiDiEiFiG"))
+if( (length(a1) %% 3) == 0 ){
+  b <- matrix(a1, ncol=3, byrow=T)
+}else{
+  b <- matrix(a1[1:(length(a1) - (length(a1) %% 3))], ncol=3, byrow=T)
+}
+
+#head(b)
+#remove [.]
+b[,3] <- sub(" [.]$", "", b[,3])
+
+#Subject
+cc <- grepl("^<http://www.wikidata.org/entity/Q[0-9]", b[,1])
+b[cc,1] <- sub("^<http://www.wikidata.org/entity/", "wd:", b[cc,1])
+b[cc,1] <- sub(">$", "", b[cc,1])
+b <- b[grepl("^wd:Q", b[,1]),]
+
+#Property
+b[,2] <- sub("<http://www.w3.org/2000/01/rdf-schema#label>", "rdfs:label", b[,2])
+b[,2] <- sub("<http://www.w3.org/2004/02/skos/core#prefLabel>", "skos:prefLabel", b[,2])
+b[,2] <- sub("<http://www.w3.org/2004/02/skos/core#altLabel>", "skos:altLabel", b[,2])
+b[,2] <- sub("<http://schema.org/description>", "schema:description", b[,2])
+b[,2] <- sub("<http://schema.org/name>", "schema:name", b[,2])
+
+#Object
+##どう書くか？？
+
+
+
+#head(b)
+#dim(b)
+#convert matrix to DF
+d <- data.frame(b)
+
+if(x == 1){
+readr::write_csv(d,
+                 file=paste0(sub(".nt", "", File_path), "_df.csv"),
+                 append=FALSE, col_names = FALSE)
+}else{
+readr::write_csv(d,
+                 file=paste0(sub(".nt", "", File_path), "_df.csv"),
+                 append=TRUE, col_names = FALSE)
+}}}
+
