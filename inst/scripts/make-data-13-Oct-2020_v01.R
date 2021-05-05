2a62e1a09b01e4e6eac74ee28b783c6e612df4d4
kumeS <satoshi.kume@riken.jp>
2021-05-06 01:33:21 +0900

    first version

--- inst/scripts/make-data-13-Oct-2020_v01.R
+++ inst/scripts/make-data-13-Oct-2020_v01.R
@@ -1,33 +1,31 @@
 ###################################################################
-#Data Dump Version: 10-Apr-2021 06:31 JST from Wikidata Dump
+#Data Dump Version: 13-Oct-2020 from Wikidata Dump
 ###################################################################
-##Download on MacOSX/LINUX Terminal
-#wget https://dumps.wikimedia.org/wikidatawiki/entities/latest-truthy.nt.bz2
-##Alternative way: Download from Public_Repository_for_AHWikiDataDbs
+##Download from Public_Repository_for_AHWikiDataDbs
 #source ./gdrive_download.sh
-#gdrive_download 1YJbbIHW9ZymAhZCyYEX25IHH8TZIC_bi ./latest-truthy-10-Apr-2021.nt.bz2
+#gdrive_download 1RVV8_NbDh7PTTFAPl1Z_p_n3cWoGyaIF ./latest-truthy-13-Oct-2020.nt.gz
 
 ##move to the filder
-#mkdir WikidataRDF-10-Apr-2021
-#mv ./latest-truthy-10-Apr-2021.nt.bz2 ./WikidataRDF-10-Apr-2021
+#mkdir WikidataRDF-13-Oct-2020
+#mv ./latest-truthy-13-Oct-2020.nt.gz ./WikidataRDF-13-Oct-2020
 
 ##Decompress the bz2 file
-#cd ./WikidataRDF-10-Apr-2021
-#bunzip2 -k latest-truthy-10-Apr-2021.nt.bz2
-#mv latest-truthy-10-Apr-2021.nt Dump.nt
+#cd ./WikidataRDF-13-Oct-2020
+#gunzip -k latest-truthy-13-Oct-2020.nt.gz
+#mv latest-truthy-13-Oct-2020.nt Dump.nt
 
 ######################################################
 ## Same processing for all files
 ######################################################
 ## Pre-Processing on MacOSX Terminal
 ##extract English labels
-#grep -e "@en ." ./Dump.nt > ./Label_en.nt
+#grep -e '"@en .' ./Dump.nt > ./Label_en.nt
 #grep -e " <http://www.w3.org/2000/01/rdf-schema#label> " ./Label_en.nt > ./Label_en_rdfs.nt
 #grep -e " <http://www.w3.org/2004/02/skos/core#altLabel> " ./Label_en.nt > ./Label_en_Altlabel.nt
 #grep -e " <http://schema.org/description> " ./Label_en.nt > ./Label_en_description.nt
 
 ##extract Japanese labels
-#grep -e "@ja ." ./Dump.nt > ./Label_ja.nt
+#grep -e '"@ja .' ./Dump.nt > ./Label_ja.nt
 #grep -e " <http://www.w3.org/2000/01/rdf-schema#label> " ./Label_ja.nt > ./Label_ja_rdfs.nt
 #grep -e " <http://www.w3.org/2004/02/skos/core#altLabel> " ./Label_ja.nt > ./Label_ja_Altlabel.nt
 #grep -e " <http://schema.org/description> " ./Label_ja.nt > ./Label_ja_description.nt
@@ -41,7 +39,7 @@
 #grep -e "^<http://www.wikidata.org/prop/direct/P[0-9]" ./Dump.nt > ./PropertyList_01.nt
 #grep -e "^<http://www.wikidata.org/entity/P[0-9]" ./Dump.nt > ./PropertyList_02.nt
 #cat PropertyList_01.nt PropertyList_02.nt > PropertyList.nt
-#grep -v -e "\"@[a-z]" ./PropertyList.nt > ./PropertyList_woat.nt
+#grep -v -e '"@[a-z]' ./PropertyList.nt > ./PropertyList_woat.nt
 #grep -e "> .$" ./PropertyList_woat.nt > ./PropertyList_woat2.nt
 #grep -e '"@en .' ./PropertyList.nt > ./PropertyList_en.nt
 #grep -e '"@ja .' ./PropertyList.nt > ./PropertyList_ja.nt
@@ -73,13 +71,13 @@
 #ls -ul -h
 
 #Chnage Dir.
-setwd("./WikidataRDF-10-Apr-2021/")
+setwd("./WikidataRDF-13-Oct-2020/")
 dir()
 
 ######################################################
 ##For Relation_P279_P31.nt
 ######################################################
-#system("open ../WikidataRDF-10-Apr-2021")
+#system("open ../WikidataRDF-13-Oct-2020")
 #source("../AHWikiDataDbs/inst/scripts/make-data_v02.R")
 File_path <- "./Relation_P279_P31.nt"
 
@@ -107,7 +105,7 @@ system.time(readRDS(file = paste0(sub(".nt", "", File_path), "_df.Rdata")))
 ######################################################
 ##For Label_en_rdfs.nt
 ######################################################
-#system("open ../WikidataRDF-10-Apr-2021")
+#system("open ../WikidataRDF-13-Oct-2020")
 #source("../AHWikiDataDbs/inst/scripts/make-data_v02.R")
 File_path <- "./Label_en_rdfs.nt"
 #Run
--- inst/scripts/make-data_v02.R
+++ inst/scripts/make-data_v02.R
@@ -62,6 +62,7 @@ readr::write_csv(d,
                  file=paste0(sub(".nt", "", File_path), "_df.csv"),
                  append=TRUE, col_names = FALSE)
 }}
+#close(con_file)
 }
 
 ############################################################
@@ -125,7 +126,9 @@ readr::write_csv(d,
 readr::write_csv(d,
                  file=paste0(sub(".nt", "", File_path), "_df.csv"),
                  append=TRUE, col_names = FALSE)
-}}}
+}}
+#close(con_file)
+}
 
 
 ############################################################
@@ -192,6 +195,7 @@ readr::write_csv(d,
                  file=paste0(sub(".nt", "", File_path), "_df.csv"),
                  append=TRUE, col_names = FALSE)
 }}
+#close(con_file)
 }
 
 ############################################################
@@ -221,7 +225,7 @@ if( (length(a1) %% 3) == 0 ){
   b <- matrix(a1[1:(length(a1) - (length(a1) %% 3))], ncol=3, byrow=T)
 }
 
-#head(b)
+#head(b); tail(b)
 # remove [.]
 b[,3] <- sub(" [.]$", "", b[,3])
 
@@ -229,21 +233,29 @@ b[,3] <- sub(" [.]$", "", b[,3])
 cc <- grepl("^<http://www.wikidata.org/prop/direct/P[0-9]", b[,1])
 b[cc,1] <- sub("^<http://www.wikidata.org/prop/direct/", "wdt:", b[cc,1])
 b[cc,1] <- sub(">$", "", b[cc,1])
+cc <- grepl("^<http://www.wikidata.org/entity/P[0-9]", b[,1])
+b[cc,1] <- sub("^<http://www.wikidata.org/entity/", "wd:", b[cc,1])
+b[cc,1] <- sub(">$", "", b[cc,1])
 
 #Property
-#cc <- grepl("^<http://www.wikidata.org/entity/Q[0-9]", b[,1])
-#b[cc,1] <- sub("^<http://www.wikidata.org/entity/", "wd:", b[cc,1])
-#b[cc,1] <- sub(">$", "", b[cc,1])
-#b <- b[grepl("^wd:Q", b[,1]),]
+for(n in 1:ncol(Prefix)){
+cc <- grepl(paste0("^", Prefix[2,n]), b[,2])
+b[cc,2] <- sub(paste0("^", Prefix[2,n]), paste0(Prefix[1,n]), b[cc,2])
+b[cc,2] <- sub(paste0(Prefix[3,n], "$"), "", b[cc,2])
+}
 
 #Object
-#cc <- grepl("^\"", b[,3])
-#b[cc,3] <- sub("^\"", "", b[cc,3])
-#cc <- grepl("\"$", b[,3])
-#b[cc,3] <- sub("\"$", "", b[cc,3])
-#table(cc)
-#head(b)
-#dim(b)
+cc <- grepl("^<http://www.wikidata.org/prop/direct/P[0-9]", b[,3])
+b[cc,3] <- sub("^<http://www.wikidata.org/prop/direct/", "wdt:", b[cc,3])
+b[cc,3] <- sub(">$", "", b[cc,3])
+cc <- grepl("^<http://www.wikidata.org/entity/Q[0-9]", b[,3])
+b[cc,3] <- sub("^<http://www.wikidata.org/entity/", "wd:", b[cc,3])
+b[cc,3] <- sub(">$", "", b[cc,3])
+cc <- grepl("^<http://www.wikidata.org/entity/P[0-9]", b[,3])
+b[cc,3] <- sub("^<http://www.wikidata.org/entity/", "wd:", b[cc,3])
+b[cc,3] <- sub(">$", "", b[cc,3])
+cc <- !grepl("<http://www.w3.org/2001/XMLSchema#decimal>$", b[,3])
+b <- b[cc,]
 
 #convert matrix to DF
 d <- data.frame(b)
@@ -257,6 +269,7 @@ readr::write_csv(d,
                  file=paste0(sub(".nt", "", File_path), "_df.csv"),
                  append=TRUE, col_names = FALSE)
 }}
+#close(con_file)
 }
 
 ############################################################
@@ -278,6 +291,18 @@ message(paste0('sum(grepl("^wd:", Dat[,3])): ', sum(grepl("^wd:", Dat[,3]))))
 saveRDS(Dat, file = paste0(sub(".nt", "", File_path), "_df.Rdata"))
 
 }
+
+
+
+Prefix <- data.frame(wd=c("wd:", "<http://www.wikidata.org/entity/", ">"),
+                     wdt=c("wdt:", "<http://www.wikidata.org/prop/direct/", ">"),
+                     wikibase=c("wikibase:", "<http://wikiba.se/ontology#", ">"),
+                     rdf=c("rdf:", "<http://www.w3.org/1999/02/22-rdf-syntax-ns#", ">"),
+                     wdtn=c("wdtn:", "<http://www.wikidata.org/prop/direct-normalized/", ">"))
+
+
+
+
 
 
 
